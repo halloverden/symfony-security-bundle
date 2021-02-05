0109d8ffce6366d5f36135af276efd402bc26924
tomme87 <tom.roar@halloverden.no>
2021-02-05 14:52:20 +0100

    ADD: AddExpressionLanguageProvidersPass for AccessDefinitionExpressionLanguageProvider

--- HalloVerdenSecurityBundle.php
+++ HalloVerdenSecurityBundle.php
@@ -4,7 +4,24 @@
 namespace HalloVerden\SecurityBundle;
 
 
+use HalloVerden\SecurityBundle\DependencyInjection\Compiler\AddExpressionLanguageProvidersPass;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
+/**
+ * Class HalloVerdenSecurityBundle
+ *
+ * @package HalloVerden\SecurityBundle
+ */
 class HalloVerdenSecurityBundle extends Bundle {
+
+  /**
+   * @inheritDoc
+   */
+  public function build(ContainerBuilder $container) {
+    parent::build($container);
+
+    $container->addCompilerPass(new AddExpressionLanguageProvidersPass());
+  }
+
 }
